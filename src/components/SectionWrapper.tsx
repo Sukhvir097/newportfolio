bbe1a28dcfdf8737c29ade4b4019fddb1005bee3
sukhvir singh <sukhvirsingh4898@gmail.com>
2025-07-11 12:06:18 -0700

    update code

--- src/components/SectionWrapper.tsx
+++ src/components/SectionWrapper.tsx
@@ -3,23 +3,23 @@
 import { motion } from "framer-motion";
 import { staggerContainer, fadeInUp } from "@/utils/motion";
 import ScrollDownIndicator from "./ScrollDownIndicator";
+import { useIsMobile } from "@/hooks/useIsMobile";
 
-type SectionWrapperProps = {
-  id: string;
-  title?: string;
-  children: React.ReactNode;
-  className?: string;
-  scrollTargetId?: string;
-};
+type SectionWrapperProps = { id: string; title?: string; children: React.ReactNode; className?: string; scrollTargetId?: string;};
 
-export default function SectionWrapper({
-  id,
-  title,
-  children,
-  className = "",
-  scrollTargetId,
+export default function SectionWrapper({ id, title, children, className = "", scrollTargetId,
 }: SectionWrapperProps) {
-  return (
+  const isMobile = useIsMobile();
+
+  return isMobile ? (
+    <section id={id} className={`pt-4 px-6 max-w-3xl mx-auto text-center space-y-4 sm:pt-12 sm:px-6 md:px-10 ${className}`}>
+      {title && (
+        <h2 className="text-xl sm:text-2xl font-semibold text-gray-800">{title}</h2>
+      )}
+      {children}
+      {scrollTargetId && <ScrollDownIndicator targetId={scrollTargetId} />}
+    </section>
+  ) : (
     <motion.section
       id={id}
       className={`pt-4 px-6 max-w-3xl mx-auto text-center space-y-4 sm:pt-12 sm:px-6 md:px-10 ${className}`}
@@ -29,16 +29,11 @@ export default function SectionWrapper({
       variants={staggerContainer}
     >
       {title && (
-        <motion.h2
-          variants={fadeInUp}
-          className="text-xl sm:text-2xl font-semibold text-gray-800"
-        >
+        <motion.h2 variants={fadeInUp} className="text-xl sm:text-2xl font-semibold text-gray-800">
           {title}
         </motion.h2>
       )}
-
       {children}
-
       {scrollTargetId && <ScrollDownIndicator targetId={scrollTargetId} />}
     </motion.section>
   );
